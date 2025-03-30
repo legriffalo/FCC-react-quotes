e27e99f677ab1a6417706b40a55b26cce39379e6
Nick-legriffalo-Grif <56260217+legriffalo@users.noreply.github.com>
2025-03-30 21:33:28 +0300

    fixed bizarre color change!

--- quotes/src/assets/components/Quote.tsx
+++ quotes/src/assets/components/Quote.tsx
@@ -16,7 +16,7 @@ interface Quoteprops {
 const Quote: React.FC<Quoteprops> = ({ id, text, author, onClick }) => {
     let query = `...%0A%0A"${text}"%0A%0A --${author}`
     return (
-        <div id = {id} className="card bg-black h-full p-5">
+        <div id = {id} className="card text-white border bg-black h-full p-5">
             <span className = "text-4xl">❝</span>
             <p className="h-6/10 lg:text-sm text-[clamp(2cqh,2.3cqh,3cqh)]  mb-[2vw]" id = "text">{text}<span>❝</span></p>
             <p className="h-1/10 text-[2cqh] lg:text-[1cqh] mb-3" id = "author"> <span className = "">-- </span>{author}</p>
